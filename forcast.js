2b851630afcce16681028a304e0e156596a18dbc
Luke Alford <luke@lukealford.com>
2015-08-21 00:30:22 +1000

    making console log for forcast.js tidy

--- forcast.js
+++ forcast.js
@@ -1,7 +1,9 @@
-
+//requirements
 var http = require("http");
+var fs = require("fs");
 var cheerio = require("cheerio");
 
+//Bom town url - change to town / region for different area
 var url = "http://www.bom.gov.au/vic/forecasts/warrnambool.shtml";
 
 
@@ -23,14 +25,36 @@ function download(url, callback) {
 
 download(url, function(data) {
   if (data) {
-    // console.log(data);
+
     var $ = cheerio.load(data);
+    //select data we want to scrape
     $(".day").each(function(i, e) {
       var day = $(e).find("h2").text();
-      var summary = $(e).find(".summary").text();
+      var summary = $(e).find("p").text();
+      var rain = $(e).find("em.rain").text();
+      var rainchance = $(e).find(".pop").text()
       var min = $(e).find(".min").text();
       var max = $(e).find(".max").text();
-      console.log(day + ", " + summary + ". Min " + min + " Max "+ max);
+      //put data into array
+      var forecastDay ={
+        day: day,
+        summary: summary,
+        rain: rain,
+        rainchance: rainchance,
+        min: min,
+        max: max
+      };
+     //stringify and parse it!
+     var s = JSON.stringify(forecastDay);
+     var p = JSON.parse(s)
+     // Get Values from JSON and display in log
+     console.log("------------------------------------------------------------")
+     console.log( p.day);
+     console.log( p.summary);
+     console.log("Chance of rain:", p.rainchance);
+     console.log( "Rain fall:",p.rain);
+     console.log( "Minimum of:",p.min);
+     console.log( "Maximum of:",p.max);
     });
   }
 });
